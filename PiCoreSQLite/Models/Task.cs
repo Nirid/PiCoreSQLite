0da4bc8a8f146fa22eac8092b49c32d1d2230ce1
Mikołaj Grabowski <firinirt@gmail.com>
2017-05-20 13:57:24 +0200

    Add project files.

---
+++ PiCoreSQLite/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PiCoreSQLite.Models
+{
+    public class Task : DbContext
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public double Difficulty { get; set; }
+        public double Duration { get; set; }
+        public string Categories { get; set; }
+        public int AssignedId { get; set; }
+        public int CompletedId { get; set; }
+        
+    }
+}
